98fba36e8cbdc7aa17920d89402526460c57f1fb
John Clary <john.clary@austintexas.gov>
2021-01-07 15:21:06 -0600

    these the scripts

--- bash_scripts/scraper_keep_alive.sh
+++ bash_scripts/scraper_keep_alive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! "$(sudo docker ps -a | grep road-conditions)" ]; then
     # container doesn't exist; create and run it
-    sudo docker run --name road-conditions --network host -d --env-file ~/atd-road-conditions/env_file -v ~/atd-road-conditions:/app atddocker/atd-road-conditions atd-road-conditions/scrape.py
+    sudo docker run --name road-conditions --network host -d --env-file /home/publisher/atd-road-conditions/env_file -v /home/publisher/atd-road-conditions:/app atddocker/atd-road-conditions atd-road-conditions/scrape.py
 elif [ ! "$(sudo docker ps | grep road-conditions)" ]; then
     # container exists but is not running; restart it
     sudo docker restart "road-conditions"
--- bash_scripts/scraper_restart.sh
+++ bash_scripts/scraper_restart.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # restart scraper to allow it to fetch any new sensor records that may have bene created
 # run this nightly
-if [ "$(sudo docker ps | grep road-conditions)" ]; then
-    # restart container if it exists
-    sudo docker restart "road-conditions"
-else
-    # create and start container if it does not exist
-    sudo docker run --name road-conditions --network host -d --env-file ~/atd-road-conditions/env_file -v ~/atd-road-conditions:/app atddocker/atd-road-conditions atd-road-conditions/scrape.py
-fi
+sudo docker restart "road-conditions"
